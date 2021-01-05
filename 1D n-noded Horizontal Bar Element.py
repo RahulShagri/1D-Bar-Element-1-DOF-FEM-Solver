4ada8bc8769ef23a97856ba03fc76ed1434aaf6e
RahulShagri <62894667+RahulShagri@users.noreply.github.com>
2021-01-05 12:33:34 +0530

    Update 1D n-noded Horizontal Bar Element.py

--- 1D n-noded Horizontal Bar Element.py
+++ 1D n-noded Horizontal Bar Element.py
@@ -3,29 +3,27 @@ import string
 
 # 1D bar element with area of cross-section, young's modulus, force at multiple nodes, displacement constraints and length of the element given
 
-elements = int(input("Enter the total number of elements in series in the system: "))
+elements = int(input("\nEnter the total number of elements in series in the system: "))
 
 # Element stiffness matrix
 
 A = np.array(input("Enter area of cross-section of the elements in sequence (ex. 120,140,130): ").split(","))
-print(A)
+A = A.astype(float)
 
 E = np.array(input("Enter the young's modulus of the elements in sequence (ex. 120,140,130): ").split(","))
-print(E)
+E = E.astype(float)
 
 L = np.array(input("Enter the length of the elements in sequence (ex. 120,140,130): ").split(","))
-print(L)
+L = L.astype(float)
 
 K = np.array([])
 
 for a, e, l in zip(A, E, L):
-    K = np.append(K, (int(a)*int(e)/int(l)))
+    K = np.append(K, a*e/l)
 
 #Assemblying the global stiffness matrix
 
 global_K = np.zeros((elements+1, elements+1))
-
-print(global_K)
 
 for i in range(elements):
     for row in range(2):
@@ -35,38 +33,71 @@ for i in range(elements):
             else:
                 global_K[row + i][column + i] -= K[i]
 
+print("\nThe global stiffness matrix is: ")
 print((global_K))
 
 # Nodal displacement matrix
 
-Q = np.array(input("Enter the known nodal displacements in sequence and enter x for unknowns (ex. 120,x,130): ").split(","))
+Q = np.array(input("\nEnter the known nodal displacements in sequence and enter x for unknowns (ex. 120,x,130): ").split(","))
 Q = Q.reshape(elements+1, 1)
-print(Q)
 
 X = np.where(Q != 'x')
 rows = np.unique(X[0]).astype(int)
-#columns = np.unique(X[1])
-print(X)
-print(rows)
 
 # Global force matrix
 
 F = np.array(input("Enter the known forces in sequence (ex. 120,140,130): ").split(","))
 F = F.reshape(elements+1, 1).astype(float)
-print(F)
+
+print("\nThe global force matrix is:")
+print((F))
 
 for i in rows:
-    print("i = " + str(i))
     for f in range(elements+1):
-        F[f] -= global_K[f][i]*int(Q[i])
+        F[f] -= global_K[f][i]*float(Q[i])
 
+F = np.delete(F, rows, 0)
+
+print("\nThe global force matrix after elimination is:")
 print((F))
 
 global_K = np.delete(global_K, rows, 0)
 global_K = np.delete(global_K, rows, 1)
 
-F = np.delete(F, rows, 0)
+print("\nThe global stiffness matrix after elimination is: ")
+print((global_K))
 
-print("Unknown nodal displacements in sequence: ")
+unknown_disp = np.linalg.solve(global_K,F)
 
-print(np.linalg.solve(global_K,F))
+i = 0
+
+for row in range(elements+1):
+    if row in rows:
+        continue
+    else:
+        Q[row] = str(0)
+        i += 1
+
+Q = Q.astype(float)
+
+i = 0
+
+for row in range(elements+1):
+    if row in rows:
+        continue
+    else:
+        Q[row] = unknown_disp[i]
+        i += 1
+
+print("\n The global nodal displacement matrix with all values is:")
+print(Q)
+
+stress = np.array([])
+
+for element in range(elements):
+    stress = np.append(stress, E[element]*((Q[element+1]-Q[element])/L[element]))
+
+stress = stress.reshape(elements, 1)
+
+print("\nThe stress matrix is:")
+print(stress)
